177afa77368cb7e3321a5ade039e65af0662c558
Victor Duran <victor.duran.em@gmail.com>
2022-10-29 22:04:14 -0500

    Set min-height of main container to screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const BackgroundImage = dynamic(() => import('../components/BackgroundImage'), {
 
 export default function Home() {
   return (
-    <div className='relative'>
+    <div className='relative min-h-screen'>
       <Head>
         <title>Pod Request Access</title>
       </Head>
