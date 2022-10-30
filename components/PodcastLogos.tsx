337a51e378836f573af9ff08d609bca180b1e802
Victor Duran <victor.duran.em@gmail.com>
2022-10-29 22:01:08 -0500

    podcast logos adjustment

--- components/PodcastLogos.tsx
+++ components/PodcastLogos.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 export default function PodcastLogos({ className }: { className?: string }) {
   return (
-    <div className='flex justify-between items-center gap-6 flex-wrap'>
+    <div className='flex items-center justify-center gap-6 max-[330px]:flex-wrap'>
       <Image src={spotifyPodcast} alt='Spotify Podcast' className={`[filter:invert(40%)_sepia(37%)_saturate(407%)_hue-rotate(187deg)_brightness(89%)_contrast(88%)] ${className}`} />
       <Image src={applePodcast} alt='Apple Podcast' className={`[filter:invert(40%)_sepia(37%)_saturate(407%)_hue-rotate(187deg)_brightness(89%)_contrast(88%)] ${className}`} />
       <Image src={googlePodcast} alt='Google Podcast' className={`[filter:invert(40%)_sepia(37%)_saturate(407%)_hue-rotate(187deg)_brightness(89%)_contrast(88%)] ${className}`} />
